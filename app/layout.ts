708f32375171f267d0a90000cc7c58b4a96056c0
Laurette Ora <133497840+OraTeta11@users.noreply.github.com>
2025-06-26 06:11:37 +0200

    chore: remove all files from repository

--- app/layout.ts
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: "University of Rwanda - Voting System",
-  description: "Official voting platform for University of Rwanda",
-    generator: 'v0.dev'
-} 
